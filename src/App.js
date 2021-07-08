3faf308c21e675a6eff7f29d5fc39edab110937d
Anjelica <ambrosio.anjelica@gmail.com>
2021-07-08 12:11:56 -0700

    updated components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+
+import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import ArticleList from './ArticleList/ArticleList';
+import Page from './Page/Page';
+import About from './About/About';
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <Header />
+        <Route exact path="/" component={ArticleList} />
 
-      <Footer />
-
+        <Footer />
       </header>
     </div>
   );
