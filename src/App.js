1b508d62ec3e0193b092d89cb60282ac64827bd6
Anjelica <ambrosio.anjelica@gmail.com>
2021-07-15 11:21:24 -0700

    built foundations for article list

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Footer from './Footer/Footer';
 import Navbar from './Navbar/Navbar';
 import Featured from './Featured/Featured';
 import About from './About/About';
+import ArticleList from './ArticleList/ArticleList'
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Header />
-      <Navbar />
-      <Featured />
-      <Route path="/about" component={About} />
-      <Footer />
-    </div>
+      <div className="App">
+        <Header />
+        <Navbar />
+        <Featured />
+        <Route exact path="/" component={ArticleList} />
+        <Route exact path="/about/" component={About} />
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/ArticleList/ArticleList.js
+++ src/ArticleList/ArticleList.js
@@ -1,1 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./ArticleList.css";
+
+function ArticleList() {
+  return (
+    <div>
+    <h1>Articles</h1>
+    </div>
+  );
+}
+
+export default ArticleList;
