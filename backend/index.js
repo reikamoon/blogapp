9a62180d1e9a1637ba3afdcd5a9ae35bb0992687
Anjelica <ambrosio.anjelica@gmail.com>
2021-07-20 15:37:53 -0700

    connected mongodb to backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,43 @@
+require("dotenv").config();
 // Initialize express
 const express = require("express");
 const app = express();
 
-// Tell our app to send the "hello world" message to our home page
+const { MongoClient } = require("mongodb");
+const mongopw = process.env.MONGO_PW;
+
+async function main() {
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = `mongodb+srv://Anjelica:${mongopw}@chatterbox.6ryms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"`;
+
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+// Placeholder
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
